f9632a61ddaa13e012f0b9fa1b74105b805e47cf
Sizwe Malobola <25reyals@gmail.com>
2020-08-14 05:35:52 -0800

    Cleaned up output

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -2,15 +2,14 @@ def bubble_sort(array)
     unsorted_array = array.dup
     (unsorted_array.length - 1).downto(0) do |i|
         0.upto( i - 1) do |x|
-            p x
             if unsorted_array[x] > unsorted_array[x + 1]
                 temp = unsorted_array[x]
                 unsorted_array[x] = unsorted_array[x + 1]
                 unsorted_array[x + 1] = temp
             end 
         end
-        p unsorted_array
     end
+    p unsorted_array
 end
 
 bubble_sort([78,5,0,2])
